ef539946bfc58bedf46f9817ba654b28a3f015d9
Cheng <67669304+kchxng@users.noreply.github.com>
2024-08-20 20:40:11 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ import conn from "./config/db.config";
 import { router } from "./routes/appRouter";
 
 /*************************************************************/
-// Desc: Server Application
+// Desc: Server Application Configuration
 /*************************************************************/
 export class App {
   public readonly app: Application;
